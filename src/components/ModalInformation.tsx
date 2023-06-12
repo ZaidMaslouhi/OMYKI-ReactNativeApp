82ee98ee48144ecbe69e12501a95c6410493e6c0
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-12 16:24:24 +0100

    feat: add ModalInformation component

---
+++ src/components/ModalInformation.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Image, ImageSourcePropType, Text, View } from "react-native";
+import Button from "./Button";
+import Colors from "../theme/colors";
+import { Fonts } from "../theme/fonts";
+
+function ModalInformation({
+  icon,
+  title,
+  description,
+  buttonText,
+  onPressButton,
+}: {
+  icon: ImageSourcePropType;
+  title: string;
+  description: string;
+  buttonText: string;
+  onPressButton: () => {};
+}) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 16,
+        paddingHorizontal: 16,
+      }}
+    >
+      <Image source={icon} />
+
+      <Text
+        style={{
+          color: Colors.dark,
+          fontFamily: Fonts.Family.brand,
+          fontSize: Fonts.Size.font20,
+          fontWeight: Fonts.Weight.bold,
+        }}
+      >
+        {title}
+      </Text>
+
+      <Text
+        style={{
+          color: Colors.neutral,
+          fontFamily: Fonts.Family.brand,
+          fontSize: Fonts.Size.font14,
+          textAlign: "center",
+        }}
+      >
+        {description}
+      </Text>
+
+      {buttonText && (
+        <Button title={buttonText} primary onPress={onPressButton} />
+      )}
+    </View>
+  );
+}
+
+export default ModalInformation;
