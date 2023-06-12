3a7c3eef21f53e3219426a2020a4d5984d457fc6
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-12 12:59:27 +0100

    chore: add theme variables colors/fonts/metrics

---
+++ src/theme/colors.js
@@ -0,0 +1,15 @@
+const Colors = {
+  brand: '#465DE7',
+  success: '#46E760',
+  warning: '#F69A12',
+  danger: '#E64646',
+  neutral: '#80889C',
+  neutral2: '#F7F6F8',
+  neutral3: '#F5F4F7',
+  white: '#FFFFFF',
+  dark: '#191B20',
+  dark50: '#191B2005',
+  dark100: '#191B2010'
+}
+
+export default Colors
---
+++ src/theme/fonts.js
@@ -0,0 +1,31 @@
+import Metrics from './metrics'
+
+const Size = {
+  font6: Metrics.screenWidth * (6 / 365),
+  font8: Metrics.screenWidth * (8 / 365),
+  font10: Metrics.screenWidth * (10 / 365),
+  font12: Metrics.screenWidth * (12 / 365),
+  font14: Metrics.screenWidth * (14 / 365),
+  font16: Metrics.screenWidth * (16 / 365),
+  font18: Metrics.screenWidth * (18 / 365),
+  font20: Metrics.screenWidth * (20 / 365),
+  font24: Metrics.screenWidth * (24 / 365)
+}
+
+const Weight = {
+  full: '900',
+  semi: '700',
+  low: '400',
+  normal: 'normal',
+  bold: 'bold'
+}
+
+const Family = {
+  brand: 'SourceSansPro'
+}
+
+export const Fonts = {
+  Size,
+  Weight,
+  Family
+}
