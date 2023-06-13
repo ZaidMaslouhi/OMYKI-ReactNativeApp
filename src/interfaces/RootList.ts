51643fbc91175ac1af5c74433ac6bbf0cce12978
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-13 15:59:12 +0100

    feat: add root stack list to navigate screens

---
+++ src/interfaces/RootList.ts
@@ -0,0 +1,17 @@
+type RootStackParamList = {
+  Home: undefined;
+  SignUp: undefined;
+  SignIn: undefined;
+  UserVerification: undefined;
+  PersonalInformation: undefined;
+  Congratulations: undefined;
+  KeyManagement: undefined;
+  GeneralSettings: undefined;
+  SharedKeys: undefined;
+  Settings: undefined;
+  MyDetails: undefined;
+  NotFound: undefined;
+  ConnectionFailed: undefined;
+};
+
+export default RootStackParamList;
