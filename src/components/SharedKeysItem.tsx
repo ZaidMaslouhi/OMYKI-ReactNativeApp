c4f5fa179187cd43ddcaffc415d51beed7988d92
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-13 11:50:18 +0100

    feat: add Shared Keys screen components

---
+++ src/components/SharedKeysItem.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Image, ImageSourcePropType, Text, View } from "react-native";
+import Colors from "../theme/colors";
+import { Fonts } from "../theme/fonts";
+
+function SharedKeysItem({
+  accessKey,
+  user,
+}: {
+  accessKey: string;
+  user: { image: ImageSourcePropType; fullName: string; email: string };
+}) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: Colors.white,
+        borderColor: Colors.dark100,
+        borderWidth: 1,
+        borderRadius: 16,
+        padding: 16,
+      }}
+    >
+      <View style={{ gap: 16 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 8,
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={user.image}
+            style={{ width: 35, height: 35, borderRadius: 35 / 2 }}
+          />
+          <View style={{ gap: 4 }}>
+            <Text
+              style={{
+                fontFamily: Fonts.Family.brand,
+                fontSize: Fonts.Size.font14,
+                color: Colors.dark,
+              }}
+            >
+              {user.fullName}
+            </Text>
+            <Text
+              style={{
+                fontFamily: Fonts.Family.brand,
+                fontSize: Fonts.Size.font12,
+                color: Colors.neutral,
+              }}
+            >
+              {user.email}
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+          }}
+        >
+          <Text
+            style={{
+              color: Colors.dark,
+              fontFamily: Fonts.Family.brand,
+              fontSize: Fonts.Size.font12,
+              backgroundColor: Colors.brand + "10",
+              paddingHorizontal: 8,
+              paddingVertical: 4,
+              borderRadius: 10,
+            }}
+          >
+            Permanent access
+          </Text>
+        </View>
+      </View>
+      <View style={{ justifyContent: "center" }}>
+        <Image source={require("../assets/icons/trash.png")} />
+      </View>
+    </View>
+  );
+}
+
+export default SharedKeysItem;
---
+++ src/screens/Key Management/SharedKeys.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import ActionBar from "../../components/ActionBar";
+import { SectionList, Text, View } from "react-native";
+import SharedKeysItem from "../../components/SharedKeysItem";
+import { Fonts } from "../../theme/fonts";
+
+const Keys = [
+  {
+    title: "Appartment 1",
+    data: [
+      {
+        key: "key 1",
+        user: {
+          fullName: "Bradley Lawlor",
+          email: "c.a.glasser@outlook.com",
+          image: require("../../assets/icons/profile.png"),
+        },
+      },
+      {
+        key: "key 2",
+        user: {
+          fullName: "Daniel Hamilton",
+          email: "jerry73@aol.com",
+          image: require("../../assets/icons/profile.png"),
+        },
+      },
+    ],
+  },
+  {
+    title: "Appartment 2",
+    data: [
+      {
+        key: "key 4",
+        user: {
+          fullName: "David Elson",
+          email: "k_pacheco@gmail.com",
+          image: require("../../assets/icons/profile.png"),
+        },
+      },
+    ],
+  },
+  {
+    title: "Appartment 3",
+    data: [
+      {
+        key: "key 5",
+        user: {
+          fullName: "James Hall",
+          email: "james.hall@gmail.com",
+          image: require("../../assets/icons/profile.png"),
+        },
+      },
+    ],
+  },
+];
+
+function SharedKeys() {
+  return (
+    <>
+      <ActionBar title="Back" withBack={true} onPress={() => () => {}} />
+
+      <SectionList
+        contentContainerStyle={{ padding: 16 }}
+        ItemSeparatorComponent={() => <View style={{ height: 8 }} />}
+        SectionSeparatorComponent={() => <View style={{ height: 8 }} />}
+        sections={Keys}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text
+            style={{
+              fontFamily: Fonts.Family.brand,
+              fontSize: Fonts.Size.font14,
+              fontWeight: Fonts.Weight.semi,
+            }}
+          >
+            {title}
+          </Text>
+        )}
+        renderItem={({ item }) => (
+          <SharedKeysItem accessKey={item.key} user={item.user} />
+        )}
+      />
+    </>
+  );
+}
+
+export default SharedKeys;
