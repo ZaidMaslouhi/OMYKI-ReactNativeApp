14a7221e560280b66510a265ea2af96b0f1f1c98
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-13 14:59:48 +0100

    feat: add all screens to the stack navigator

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignUp from "../screens/Auth/SignUp";
 import { INIT_ROUTE } from "../utils/constants";
+import SignIn from "../screens/Auth/SignIn";
+import UserVerification from "../screens/Auth/UserVerification";
+import PersonalInformation from "../screens/Auth/PersonalInformation";
+import Congratulations from "../screens/Auth/Congratulations";
+import KeyManagement from "../screens/Key Management/KeyManagement";
+import GeneralSettings from "../screens/Key Management/GeneralSettings";
+import SharedKeys from "../screens/Key Management/SharedKeys";
+import { Settings } from "react-native";
+import MyDetails from "../screens/Settings/MyDetails";
+import NotFound from "../screens/Error/NotFound";
+import ConnectionFailed from "../screens/Error/ConnectionFailed";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +23,20 @@ function Navigator() {
       screenOptions={{ headerShown: false }}
     >
       <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="UserVerification" component={UserVerification} />
+      <Stack.Screen
+        name="PersonalInformation"
+        component={PersonalInformation}
+      />
+      <Stack.Screen name="Congratulations" component={Congratulations} />
+      <Stack.Screen name="KeyManagement" component={KeyManagement} />
+      <Stack.Screen name="GeneralSettings" component={GeneralSettings} />
+      <Stack.Screen name="SharedKeys" component={SharedKeys} />
+      <Stack.Screen name="Settings" component={Settings} />
+      <Stack.Screen name="MyDetails" component={MyDetails} />
+      <Stack.Screen name="NotFound" component={NotFound} />
+      <Stack.Screen name="ConnectionFailed" component={ConnectionFailed} />
     </Stack.Navigator>
   );
 }
