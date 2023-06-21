f43978bfc9f60e735024f1120671f0514b3cc5bb
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-21 10:27:47 +0100

    feat: add user auth custom hook

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
+import "expo-dev-client";
 import "react-native-gesture-handler";
-import React, { useCallback } from "react";
 import * as SplashScreen from "expo-splash-screen";
+import React, { useCallback } from "react";
 import { StyleSheet, View } from "react-native";
 import useCustomFonts from "./src/hooks/useCustomFonts";
-import Colors from "./src/theme/colors";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigator from "./src/navigation";
+import Colors from "./src/theme/colors";
+import { useAuthentication } from "./src/hooks/useAuthentication";
 
 export default function App() {
   const fontsLoaded = useCustomFonts();
+  const { user } = useAuthentication();
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
