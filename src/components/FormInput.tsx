8302aba3d04cce7a5ba27ffaea9348927c7a288f
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-24 15:31:40 +0100

    feat: add error message to form input

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -6,11 +6,13 @@ import { Fonts } from "../theme/fonts";
 function FormInput({
   label,
   icon,
+  errorMessage,
   children,
 }: {
   label: string;
-  children: ReactElement;
   icon?: ReactNode;
+  errorMessage?: string;
+  children: ReactElement;
 }) {
   return (
     <View style={{ gap: 10 }}>
@@ -22,23 +24,30 @@ function FormInput({
       >
         {label}
       </Text>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          gap: 5,
-          height: 55,
-          borderWidth: 2,
-          borderColor: Colors.dark100,
-          borderRadius: 16,
-          backgroundColor: Colors.white,
-          paddingHorizontal: 16,
-          paddingVertical: 18,
-          overflow: "hidden",
-        }}
-      >
-        {icon && icon}
-        {children}
+      <View style={{ gap: 8 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            height: 55,
+            borderWidth: 2,
+            borderColor: Colors.dark100,
+            borderRadius: 16,
+            backgroundColor: Colors.white,
+            paddingHorizontal: 16,
+            paddingVertical: 18,
+            overflow: "hidden",
+          }}
+        >
+          {icon && icon}
+          {children}
+        </View>
+        {errorMessage && (
+          <Text style={{ color: Colors.danger, fontSize: Fonts.Size.font12 }}>
+            {errorMessage}
+          </Text>
+        )}
       </View>
     </View>
   );
