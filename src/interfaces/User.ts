0648d79ac6bee645f8f9047bbb69d7b379ec70c6
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-24 15:59:40 +0100

    feat: add user interface properties

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,10 +1,13 @@
 import { ImageSourcePropType } from "react-native";
 
 interface User {
-  image: ImageSourcePropType;
-  fullName: string;
+  id: string;
+  firstName: string;
+  lastName: string;
   email: string;
+  phoneNumber: string;
+  indicativeNumber: string;
+  image: ImageSourcePropType;
 }
-
 
 export default User;
