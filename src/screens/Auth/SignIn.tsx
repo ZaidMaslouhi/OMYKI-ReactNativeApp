37a1c4b33d1652e9567cba942dfb1d4b74101a15
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-24 16:05:51 +0100

    feat: add google sign in/store user local storage

--- src/screens/Auth/SignIn.tsx
+++ src/screens/Auth/SignIn.tsx
@@ -1,14 +1,79 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+  GoogleAuthProvider,
+  UserInfo,
+  onAuthStateChanged,
+  signInWithCredential,
+} from "firebase/auth";
+import { auth } from "../../config/firebase";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Button from "../../components/Button";
 import LogoTitle from "../../components/LogoTitle";
 import PhoneNumberInput from "../../components/PhoneNumberInput";
 import Colors from "../../theme/colors";
 import { Fonts } from "../../theme/fonts";
-import { useNavigation } from "@react-navigation/native";
+import GoogleIcon from "../../assets/icons/Google.svg";
+import AppleIcon from "../../assets/icons/Apple.svg";
+import { GoogleSigninConfigure } from "../../config/googleAuth";
 
 function SignIn() {
   const navigation = useNavigation();
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const [request, response, promptAsync] = Google.useAuthRequest(
+    GoogleSigninConfigure
+  );
+
+  const getLocalUser = async () => {
+    try {
+      setLoading(true);
+      const userJSON = await AsyncStorage.getItem("@user");
+      const userData = userJSON ? JSON.parse(userJSON) : null;
+      setUserInfo(userData);
+    } catch (e) {
+      console.log(e, "Error getting local user");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getLocalUser();
+    const unsub = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        await AsyncStorage.setItem("@user", JSON.stringify(user));
+      }
+    });
+    return () => unsub();
+  }, []);
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential);
+    }
+  }, [response]);
+
+  useEffect(() => {
+    //TODO ADD VERIFICATION FROM API IF USER ALREADY HAVE A PHONE NUMBER
+    if (userInfo && !userInfo.phoneNumber) {
+      console.log(userInfo);
+      navigation.navigate("PersonalInformation", { userInfo });
+    }
+  }, [navigation, userInfo]);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   return (
     <View
@@ -20,8 +85,8 @@ function SignIn() {
       }}
     >
       <LogoTitle
-        logo={require("../../assets/icons/omyki-logo.gif")}
-        title={"Sign In to Omyki"}
+        logo={require("../../assets/images/OmykiLogo.gif")}
+        title="Sign In to Omyki"
       />
 
       <PhoneNumberInput />
@@ -47,14 +112,13 @@ function SignIn() {
 
         <Button
           title="Sign In with Google"
-          icon={require("../../assets/icons/google-icon.png")}
-          onPress={() => navigation.navigate("UserVerification")}
+          icon={<GoogleIcon />}
+          onPress={() => promptAsync()}
         />
-
         <Button
           title="Sign In with Apple"
-          icon={require("../../assets/icons/apple-icon.png")}
-          onPress={() => navigation.navigate("UserVerification")}
+          icon={<AppleIcon />}
+          onPress={() => promptAsync()}
         />
       </View>
     </View>
