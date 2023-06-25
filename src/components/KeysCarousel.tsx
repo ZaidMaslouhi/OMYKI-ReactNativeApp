59e2be91726f8cc8f5c940e89e161f78ef8ef0e4
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-25 01:44:40 +0100

    feat: change keys slider to flat list

--- src/components/KeysCarousel.tsx
+++ src/components/KeysCarousel.tsx
@@ -1,6 +1,5 @@
-import React, { Component, useRef, useState } from "react";
-import { FlatListProps, View } from "react-native";
-import Carousel, { Pagination } from "react-native-snap-carousel";
+import React from "react";
+import { FlatList, TouchableOpacity, View } from "react-native";
 import Colors from "../theme/colors";
 import Metrics from "../theme/metrics";
 import Key from "../interfaces/Key";
@@ -8,9 +7,9 @@ import CarouselCardItem from "./CarouselCardItem";
 
 export const ITEM_WIDTH = Math.round(Metrics.screenWidth * 0.75);
 
-const Keys: Key[] = [
+const KeysList: Key[] = [
   {
-    device: "Aenean leo",
+    device: "Aeneane leo",
     title: "Residence Mont Calm 1",
     favorite: true,
     imgUrl: require("../assets/images/ResidenceMontCalm1.jpg"),
@@ -29,9 +28,6 @@ const Keys: Key[] = [
 ];
 
 function KeysCarousel() {
-  const [index, setIndex] = useState(0);
-  const carouselRef = useRef(null);
-
   return (
     <View
       style={{
@@ -41,39 +37,26 @@ function KeysCarousel() {
         borderBottomRightRadius: 60,
       }}
     >
-      <Carousel
-        layout="default"
-        ref={carouselRef}
-        data={Keys}
-        renderItem={({ item }) => (
-          <CarouselCardItem item={item} width={ITEM_WIDTH} />
-        )}
-        sliderWidth={Metrics.screenWidth}
-        itemWidth={ITEM_WIDTH}
-        onSnapToItem={(index: number) => setIndex(index)}
-        useScrollView={true}
-        loop={true}
-      />
-
-      <Pagination
-        dotsLength={Keys.length}
-        activeDotIndex={index}
-        // carouselRef={carouselRef}
-        // tappableDots={true}
-        containerStyle={{ paddingVertical: 0 }}
-        dotStyle={{
-          width: 12,
-          height: 8,
-          borderRadius: 6,
-          marginHorizontal: 0,
-          backgroundColor: Colors.white,
+      <FlatList
+        data={KeysList}
+        horizontal
+        snapToInterval={ITEM_WIDTH + 16}
+        decelerationRate="fast"
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableOpacity
+              activeOpacity={1}
+              style={{
+                marginLeft: 24,
+                marginRight: index === KeysList.length - 1 ? 24 : 0,
+              }}
+            >
+              <CarouselCardItem item={item} width={ITEM_WIDTH} />
+            </TouchableOpacity>
+          );
         }}
-        inactiveDotStyle={{
-          width: 8,
-          opacity: 0.1,
-        }}
-        inactiveDotScale={1}
-        animatedDuration={300}
       />
     </View>
   );
