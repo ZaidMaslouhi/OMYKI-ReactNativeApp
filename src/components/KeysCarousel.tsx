59e58d154b5dd56c64f5f4244fead15f8c7764c6
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-27 18:12:58 +0100

    feat: add active selected place to carousel

--- src/components/KeysCarousel.tsx
+++ src/components/KeysCarousel.tsx
@@ -7,7 +7,13 @@ import Place from "../interfaces/Place";
 
 export const ITEM_WIDTH = Math.round(Metrics.screenWidth * 0.75);
 
-function KeysCarousel({ places }: { places: Place[] }) {
+function KeysCarousel({
+  places,
+  activePlace,
+}: {
+  places: Place[];
+  activePlace: (index: number) => void;
+}) {
   return (
     <View
       style={{
@@ -27,10 +33,14 @@ function KeysCarousel({ places }: { places: Place[] }) {
         renderItem={({ item, index }) => {
           return (
             <TouchableOpacity
+              key={index}
               activeOpacity={1}
               style={{
                 marginLeft: 24,
                 marginRight: index === places.length - 1 ? 24 : 0,
+              }}
+              onPress={() => {
+                activePlace(index);
               }}
             >
               <CarouselCardItem item={item} width={ITEM_WIDTH} />
