633eda0cf45722fcc72573e5950248418759de56
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-06-29 23:54:13 +0100

    feat: add loading indicator for loading state

--- src/screens/Key Management/Keychain.tsx
+++ src/screens/Key Management/Keychain.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FlatList, View, Text } from "react-native";
+import { FlatList } from "react-native";
 import ModalInformation from "../../components/ModalInformation";
 import ShareButton from "../../components/ShareButton";
 import ActionBar from "../../components/ActionBar";
@@ -8,26 +8,19 @@ import KeyCard from "../../components/KeyCard";
 import Place from "../../interfaces/Place";
 import { useQuery } from "react-query";
 import { GetAllPlacesByUser } from "../../services/place";
-import Colors from "../../theme/colors";
+import LoadingIndicator from "../../components/LoadingIndicator";
 
 function Keychain() {
-  const {
-    data: Places,
-    isSuccess,
-    isLoading,
-    isError,
-  } = useQuery<unknown, string, Place[]>("places", GetAllPlacesByUser);
+  const { isLoading, data: Places } = useQuery<unknown, string, Place[]>(
+    "places",
+    GetAllPlacesByUser
+  );
 
   const [activePlace, setActivePlace] = useState<Place | null>(null);
 
   if (isLoading) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ color: Colors.brand }}>Loading...</Text>
-      </View>
-    );
-  } else if (isSuccess) console.log(Places);
-  else if (isError) console.log("ERROR");
+    return <LoadingIndicator />;
+  }
 
   return (
     <>
@@ -42,14 +35,13 @@ function Keychain() {
           <FlatList
             numColumns={2}
             data={activePlace?.actions || Places[0].actions}
-            // data={Places[0].actions}
             contentContainerStyle={{ padding: 8 }}
             keyExtractor={(item, index) => item.name + index}
             renderItem={({ item, index }) => {
               return (
                 <KeyCard
                   key={item.name + index}
-                  keyName={item.name}
+                  action={item}
                   isLocked={index % 2 == 0}
                   isNearby={index === 0}
                 />
