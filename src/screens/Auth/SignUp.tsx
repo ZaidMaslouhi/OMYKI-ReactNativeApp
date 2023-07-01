61e164420b7ec61cd8f7c34e4c251ec993090366
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-07-01 11:31:11 +0100

    feat: add form validation to signup screen

--- src/screens/Auth/SignUp.tsx
+++ src/screens/Auth/SignUp.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
 import Button from "../../components/Button";
 import PhoneNumberInput from "../../components/PhoneNumberInput";
@@ -8,9 +8,30 @@ import { Fonts } from "../../theme/fonts";
 import { useNavigation } from "@react-navigation/native";
 import GoogleIcon from "../../assets/icons/Google.svg";
 import AppleIcon from "../../assets/icons/Apple.svg";
+import * as yup from "yup";
+import { Formik } from "formik";
+
+const validationSchema = yup.object({
+  phoneNumber: yup.string().required(),
+  indicativeNumber: yup.string().required(),
+});
 
 function SignUp() {
   const navigation = useNavigation();
+  const [values, setValues] = useState<yup.InferType<typeof validationSchema>>({
+    phoneNumber: "",
+    indicativeNumber: "33",
+  });
+
+  const handleSubmitForm = async (values: any) => {
+    const validatedValues = await validationSchema.validate(values);
+    if (validatedValues) {
+      console.log(validatedValues);
+      // navigation.navigate("UserVerification");
+    } else {
+      console.error("Invalid form data!"); // Show validation errors
+    }
+  };
 
   return (
     <View
@@ -26,13 +47,29 @@ function SignUp() {
         title={"Sign Up to Omyki"}
       />
 
-      <PhoneNumberInput />
-
-      <Button
-        title="Continue"
-        primary
-        onPress={() => navigation.navigate("UserVerification")}
-      />
+      <Formik
+        initialValues={{ ...values }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmitForm}
+      >
+        {({ values, errors, handleSubmit, handleChange }) => (
+          <>
+            <PhoneNumberInput
+              phoneNumber={{
+                number: values.phoneNumber,
+                indicative: values.indicativeNumber,
+              }}
+              handleChangeNumber={handleChange("phoneNumber")}
+              handleChangeCallingCode={handleChange("indicativeNumber")}
+              errorMessage={
+                errors.phoneNumber?.toString() ||
+                errors.indicativeNumber?.toString()
+              }
+            />
+            <Button title="Continue" primary onPress={() => handleSubmit()} />
+          </>
+        )}
+      </Formik>
 
       <View style={{ gap: 16 }}>
         <View style={{ alignItems: "center" }}>
