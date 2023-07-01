74bdb08b8192b95f839196ad9791adfa98df3b36
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-07-01 11:43:56 +0100

    feat: add activeItem state to horizontal list

--- src/components/HorizontalList.tsx
+++ src/components/HorizontalList.tsx
@@ -1,8 +1,18 @@
-import React from "react";
-import { FlatList } from "react-native";
-import HorizontalListItem from "./HorizontalListItem";
+import React, { useState } from "react";
+import { FlatList, TouchableHighlight, Text } from "react-native";
+import Action from "../interfaces/Action";
+import Colors from "../theme/colors";
+import { Fonts } from "../theme/fonts";
 
-function HorizontalList({ items }: { items: any[] }) {
+function HorizontalList({
+  items,
+  onChange,
+}: {
+  items: Action[];
+  onChange: (id: string) => void;
+}) {
+  const [activeItem, setActiveItem] = useState(0);
+
   return (
     <FlatList
       horizontal
@@ -11,7 +21,32 @@ function HorizontalList({ items }: { items: any[] }) {
       showsHorizontalScrollIndicator={false}
       keyExtractor={(_, index) => index.toString()}
       renderItem={({ item, index }) => {
-        return <HorizontalListItem key={index} title={item} />;
+        return (
+          <TouchableHighlight
+            style={{
+              paddingVertical: 8,
+              paddingHorizontal: 16,
+              borderWidth: 2,
+              borderColor:
+                activeItem === index ? Colors.brand : Colors.neutral3,
+              borderRadius: 16,
+            }}
+            onPress={() => {
+              setActiveItem(index);
+              onChange(item.id);
+            }}
+          >
+            <Text
+              style={{
+                color: activeItem === index ? Colors.brand : Colors.dark,
+                fontFamily: Fonts.Family.brand,
+                fontSize: Fonts.Size.font14,
+              }}
+            >
+              {item.name}
+            </Text>
+          </TouchableHighlight>
+        );
       }}
     />
   );
